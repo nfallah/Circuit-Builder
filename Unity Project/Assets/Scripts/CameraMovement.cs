ac97a28ce7dd6675c83ab09b660c960a00c9e7f7
Nima Fallah <nfallah399@gmail.com>
2023-06-23 01:42:13 -0400

    Added mouse movement controls & coordinates
    Mouse movement includes: holding LMB to move relative to the mouse and the scroll wheel to move up and down relative to the camera/mouse ray
    
    Coordinates are displayed at the top left

--- Unity Project/Assets/Scripts/CameraMovement.cs
+++ Unity Project/Assets/Scripts/CameraMovement.cs
@@ -2,45 +2,91 @@
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] float movementSpeed, minHeight, maxHeight;
+    [SerializeField] Camera playerCamera;
+
+    [SerializeField] float movementSpeed, scrollSpeed, minHeight, maxHeight;
 
     [SerializeField] KeyCode upKey, downKey;
 
-    private Vector3 cameraPos; // Utilized for height clamping
+    private readonly Plane raycastPlane = new Plane(Vector3.up, Vector3.zero);
+
+    private Vector3 mousePos; // Keeps track of the mouse position from the previous frame
 
     private void Start()
     {
-        cameraPos = transform.position;
-        UpdatePos();
+        ClampPos();
+        UpdateMousePos();
     }
 
     private void Update()
     {
-        // Obtains x and z axis values based on input; initializes y-axis value
-        float x = Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime;
-        float y = 0;
-        float z = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
+        float x, y, z;
 
-        // Determines y axis values (holding both "upKey" and "downKey" will negate one another)
-        if (Input.GetKey(upKey))
+        Vector3 mousePosPrev = mousePos;
+
+        UpdateMousePos();
+
+        if (Input.GetMouseButton(0))
         {
-            y += movementSpeed * Time.deltaTime;
+            Vector3 mousePosDelta = mousePosPrev - mousePos;
+
+            x = mousePosDelta.x;
+            z = mousePosDelta.z;
         }
 
-        if (Input.GetKey(downKey))
+        else
         {
-            y -= movementSpeed * Time.deltaTime;
+            // Obtains x and z axis values based on input
+            x = Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime;
+            z = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;
+        }
+
+        if (Mathf.Abs(Input.mouseScrollDelta.y) > 0)
+        {
+            y = -Input.mouseScrollDelta.y * scrollSpeed * Time.deltaTime;
+        }
+
+        else
+        {
+            y = 0;
+
+            // Determines y axis values (holding both "upKey" and "downKey" will negate one another)
+            if (Input.GetKey(upKey))
+            {
+                y += movementSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey(downKey))
+            {
+                y -= movementSpeed * Time.deltaTime;
+            }
         }
 
         // Adds obtained values and updates position
-        cameraPos += x * Vector3.right + y * Vector3.up + z * Vector3.forward;
-        UpdatePos();
+        transform.position += x * Vector3.right + y * -CameraRay.direction + z * Vector3.forward;
+        ClampPos();
+        UpdateMousePos();
     }
 
-    // Clamps values and mirrors "cameraPos" to the actual in-scene transform
-    private void UpdatePos()
+    // Clamps values to ensure the user cannot traverse out of bounds
+    private void ClampPos()
     {
-        cameraPos.y = Mathf.Clamp(cameraPos.y, minHeight, maxHeight);
-        transform.position = cameraPos;
+        Vector3 pos = transform.position;
+
+        pos.y = Mathf.Clamp(pos.y, minHeight, maxHeight);
+        transform.position = pos;
     }
+
+    private void UpdateMousePos()
+    {
+        Ray ray = CameraRay;
+
+        if (raycastPlane.Raycast(ray, out float distance))
+        {
+            mousePos = ray.GetPoint(distance);
+            Coordinates.Instance.UpdateCoordinates(mousePos);
+        }
+    }
+
+    private Ray CameraRay { get { return playerCamera.ScreenPointToRay(Input.mousePosition); } }
 }
---
+++ Unity Project/Assets/Scripts/Coordinates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Coordinates : MonoBehaviour
+{
+    private static Coordinates instance; // Ensures a singleton state pattern is maintained
+
+    [SerializeField] TextMeshProUGUI coordinateText;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this);
+            throw new Exception("Coordinates instance already established; terminating.");
+        }
+
+        instance = this;
+    }
+
+    public void UpdateCoordinates(Vector3 worldPos)
+    {
+        coordinateText.text = "(" + worldPos.x.ToString("0.0") + ", " + worldPos.z.ToString("0.0") + ")";
+    }
+
+    // Getter method
+    public static Coordinates Instance { get { return instance; } }
+}
