69341291258206dcd8add18c394845ce4fcac423
Nima Fallah <nfallah399@gmail.com>
2023-07-05 02:11:51 -0400

    Added FPS counter
    Compass UI disabled for now until a good alternative sprite is found

---
+++ Unity Project/Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI fpsText;
+
+    private float fps;
+
+    private void Update()
+    {
+        fps = (int)(Time.timeScale / Time.smoothDeltaTime);
+        fpsText.text = "FPS: " + fps;
+    }
+}
