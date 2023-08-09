63151e00e5234fdcedbfe96f7094e6326e653a7a
Nima Fallah <nfallah399@gmail.com>
2023-08-09 00:31:19 -0400

    Organized & documented scripts

--- Unity Project/Assets/Scripts/MenuInterfaceManager.cs
+++ Unity Project/Assets/Scripts/MenuInterfaceManager.cs
@@ -3,28 +3,99 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// MenuInterfaceManager handles all UI interactions and transitions within the menu scene.
+/// </summary>
 public class MenuInterfaceManager : MonoBehaviour
 {
+    // Singleton state reference
     private static MenuInterfaceManager instance;
 
-    [SerializeField] Color defaultColor, saveColor;
+    /// <summary>
+    /// The colors of save slots when uncreated and created. respectively.
+    /// </summary>
+    [SerializeField]
+    Color defaultColor,
+        saveColor;
 
+    /// <summary>
+    /// How much the height of <seealso cref="deleteErrorTransform"/> should increase for each additional error message.
+    /// </summary>
     [SerializeField] float deleteErrorMessageSize;
 
+    /// <summary>
+    /// Attempts to exit the <seealso cref="currentInterface"/> in use.<br/><br/>
+    /// For some interfaces, this will not instantly occur (such as going back to the options menu from the guide).
+    /// </summary>
     [SerializeField] KeyCode cancelKey;
 
-    [SerializeField] GameObject deleteErrorInterface, customCircuitPanel, customCircuitPrefab, optionSelectionInterface, guideInterface, customCircuitsInterface, optionsInterface, sceneDeletionInterface, sceneNameInterface, transparentBackground, transparentBackgroundUpper;
+    /// <summary>
+    /// Utilized to add and reference custom circuits from the directory. 
+    /// </summary>
+    [SerializeField]
+    GameObject customCircuitPanel,
+        customCircuitPrefab;
 
-    [SerializeField] TMP_InputField sceneNameInputField;
+    /// <summary>
+    /// Loaded in when a relevant interface is opened.<br/><br/>
+    /// Within the scene, this should be a semi-transparent background overlayed onto everything except the currently opened interface.
+    /// </summary>
+    [SerializeField]
+    GameObject transparentBackground,
+        transparentBackgroundUpper; // UtilizedfFor multi-level interfaces such as the options interface.
 
-    [SerializeField] RectTransform deleteErrorTransform;
+    [Space(10)]
+    /// <summary>
+    /// List of all interfaces within the scene.
+    /// </summary>
+    [SerializeField]
+    GameObject customCircuitsInterface, // Displays all currently open custom circuits and allows the user to view and/or delete them; opened from the options menu.
+        deleteErrorInterface, // The error log(s) displayed when a custom circuit cannot be deleted for any reason(s).
+        guideInterface, // Displays the guide prefab; opened from the options menu.
+        optionSelectionInterface, // Allows the user to activate customCircuitsInterface as well as guideInterface.
+        optionsInterface, // The parent of optionSelectionInterface indicating that the options menu is open.
+        sceneDeletionInterface, // Prompts the user to confirm deleting an editor structure (save slot).
+        sceneNameInterface; // Prompts the user to compose a name for an editor structure (save slot).
 
-    [SerializeField] TextMeshProUGUI deleteErrorText, save1, save2, save3, sceneNameError;
+    [Space(10)]
+    /// <summary>
+    /// Utilized to expand the vertical height of the error interface by factors of <seealso cref="deleteErrorMessageSize"/>.
+    /// </summary>
+    [SerializeField]
+    RectTransform deleteErrorTransform;
 
+    /// <summary>
+    /// The text components for the delete and scene creation interfaces utilized to display the cause(s) of error.
+    /// </summary>
+    [SerializeField]
+    TextMeshProUGUI deleteErrorText,
+    sceneNameError;
+
+    /// <summary>
+    /// The input field within <seealso cref="sceneNameInterface"/> that the user uses to compose a scene name.
+    /// </summary>
+    [SerializeField]
+    TMP_InputField sceneNameInputField;
+
+    /// <summary>
+    /// Text components of all 3 save slots within the menu used for setting and deleting their names.
+    /// </summary>
+    [SerializeField]
+    TextMeshProUGUI save1,
+        save2,
+        save3;
+
+    /// <summary>
+    /// The current UI interface in use.
+    /// </summary>
     private GameObject currentInterface;
 
+    /// <summary>
+    /// Index of the current <see cref="EditorStructure"/> loaded into the scene.
+    /// </summary>
     private int currentSceneIndex = -1;
 
+    // Enforces a singleton state pattern
     private void Awake()
     {
         if (instance != null)
@@ -36,6 +107,7 @@ public class MenuInterfaceManager : MonoBehaviour
         instance = this;
     }
 
+    // Loads in all serialized editor scenes and preview structures.
     private void Start()
     {
         UpdateInterface();
@@ -46,42 +118,51 @@ public class MenuInterfaceManager : MonoBehaviour
 
     private void Update()
     {
+        // Default exit controls for all interfaces except the options interface.
         if (currentInterface != optionsInterface)
         {
             if (Input.GetKeyDown(cancelKey) || Input.GetMouseButtonDown(1)) CancelCurrentSubmission();
         }
 
-        else
+        // Otherwise, the options interface is opened and should utilize a different transition scheme.
+        else if (Input.GetKeyDown(cancelKey) || Input.GetMouseButtonDown(1))
         {
-            if (Input.GetKeyDown(cancelKey) || Input.GetMouseButtonDown(1))
+            // If at the root, exit the interface.
+            if (optionSelectionInterface.activeSelf) CancelCurrentSubmission();
+
+            // If within the custom circuits interface whilst the delete error interface is not open, return to the root.
+            else if (customCircuitsInterface.activeSelf && !deleteErrorInterface.activeSelf)
             {
-                if (optionSelectionInterface.activeSelf)
-                {
-                    CancelCurrentSubmission();
-                }
+                customCircuitsInterface.SetActive(false);
+                optionSelectionInterface.SetActive(true);
+            }
 
-                else if (customCircuitsInterface.activeSelf && !deleteErrorInterface.activeSelf)
-                {
-                    customCircuitsInterface.SetActive(false);
-                    optionSelectionInterface.SetActive(true);
-                }
+            // If within the guide interface, return to the root.
+            else if (guideInterface.activeSelf)
+            {
+                guideInterface.SetActive(false);
+                optionSelectionInterface.SetActive(true);
+            }
 
-                else if (guideInterface.activeSelf)
-                {
-                    guideInterface.SetActive(false);
-                    optionSelectionInterface.SetActive(true);
-                }
-
-                else if (deleteErrorInterface.activeSelf)
-                {
-                    transparentBackgroundUpper.SetActive(false);
-                    transparentBackground.SetActive(true);
-                    deleteErrorInterface.SetActive(false);
-                }
+            // If within the guide delete error interface, re-adjust background layers to make the custom circuit interface accessible.
+            else if (deleteErrorInterface.activeSelf)
+            {
+                transparentBackgroundUpper.SetActive(false);
+                transparentBackground.SetActive(true);
+                deleteErrorInterface.SetActive(false);
             }
         }
     }
 
+    /// <summary>
+    /// Opens an editor scene; called by pressing one of the valid save buttons.
+    /// </summary>
+    /// <param name="sceneIndex"></param>
+    public void OpenScene(int sceneIndex) { MenuLogicManager.Instance.OpenScene(sceneIndex); }
+
+    /// <summary>
+    /// Instantiates a <seealso cref="customCircuitPrefab"/> button for each preview structure.
+    /// </summary>
     private void AddCustomBookmarks()
     {
         List<PreviewStructure> previewStructures = MenuSetupManager.Instance.PreviewStructures;
@@ -91,27 +172,31 @@ public class MenuInterfaceManager : MonoBehaviour
             GameObject current = Instantiate(customCircuitPrefab, customCircuitPanel.transform);
 
             current.GetComponentInChildren<TextMeshProUGUI>().text = previewStructure.Name;
+
+            // Adds the relevant listeners to ensure the custom circuit can be deleted and viewed by their respective buttons.
             current.GetComponent<CustomCircuitButtons>().DeleteButton.onClick.AddListener(delegate { DeletePreview(current, previewStructure); });
             current.GetComponent<CustomCircuitButtons>().ViewButton.onClick.AddListener(delegate { PreviewScene(previewStructure); });
         }
     }
 
-    // Called by pressing one of the scene buttons
-    public void OpenScene(int sceneIndex)
-    {
-        MenuLogicManager.Instance.OpenScene(sceneIndex);
-    }
-
+    /// <summary>
+    /// Deletes a preview structure from the game and directory given it is not in use.
+    /// </summary>
+    /// <param name="button">The button assigned this method by a delegate.</param>
+    /// <param name="previewStructure">The preview structure to attempt deletion on.</param>
     public void DeletePreview(GameObject button, PreviewStructure previewStructure)
     {
+        // Obtains a list of all error messages in an attempt to delete the preview structure
         List<string> errorMessages = MenuLogicManager.CanDeleteCustomCircuit(previewStructure);
 
+        // If there are no errors, proceed with deletion.
         if (errorMessages.Count == 0)
         {
             MenuSetupManager.Instance.DeletePreviewStructure(previewStructure);
             Destroy(button);
         }
 
+        // Otherwise, open the delete error interface and display errors.
         else
         {
             transparentBackgroundUpper.SetActive(true);
@@ -122,6 +207,7 @@ public class MenuInterfaceManager : MonoBehaviour
 
             int index = 0;
 
+            // Adds each error
             foreach (string errorMessage in errorMessages)
             {
                 index++;
@@ -130,6 +216,9 @@ public class MenuInterfaceManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Referenced when the user acknowledges and closes the delete error interface; called by pressing the in-scene button.
+    /// </summary>
     public void OnCircuitDeleteErrorConfirm()
     {
         transparentBackgroundUpper.SetActive(false);
@@ -137,11 +226,16 @@ public class MenuInterfaceManager : MonoBehaviour
         deleteErrorInterface.SetActive(false);
     }
 
-    public void PreviewScene(PreviewStructure previewStructure)
-    {
-        MenuLogicManager.Instance.OpenPreview(previewStructure);
-    }
+    /// <summary>
+    /// Opens a preview structure whose internal components the user wishes to inspect.<br/><br/>
+    /// This method is assigned as a listener to the delete button within each instantiated <seealso cref="customCircuitPrefab"/>.
+    /// </summary>
+    /// <param name="previewStructure">The preview structure to open.</param>
+    public void PreviewScene(PreviewStructure previewStructure) { MenuLogicManager.Instance.OpenPreview(previewStructure); }
 
+    /// <summary>
+    /// Sets the names of all save slots corresponding to serialized editor structures.
+    /// </summary>
     public void UpdateInterface()
     {
         EditorStructure[] editorStructures = MenuSetupManager.Instance.EditorStructures;
@@ -153,6 +247,10 @@ public class MenuInterfaceManager : MonoBehaviour
         if (editorStructures[2] != null) { save3.text = editorStructures[2].Name; save3.color = saveColor; }
     }
 
+    /// <summary>
+    /// Opens <seealso cref="sceneDeletionInterface"/> prompting a user to acknowledge their action to delete a editor structure (save slot).
+    /// </summary>
+    /// <param name="sceneIndex">The index of the prospective editor structure to delete</param>
     public void BeginSceneDeletion(int sceneIndex)
     {
         if (MenuSetupManager.Instance.EditorStructures[sceneIndex] == null) return;
@@ -161,30 +259,46 @@ public class MenuInterfaceManager : MonoBehaviour
         BeginInterface(sceneDeletionInterface);
     }
 
+    /// <summary>
+    /// Opens <seealso cref="sceneNameInterface"/> prompting a user to compose a name to create an editor structure.
+    /// </summary>
+    /// <param name="sceneIndex">The index of the prospective editor structure to create.</param>
     public void BeginSceneNameSubmission(int sceneIndex)
     {
         currentSceneIndex = sceneIndex;
         BeginInterface(sceneNameInterface);
     }
 
+    /// <summary>
+    /// Opens the options interface; called by pressing an in-scene button.
+    /// </summary>
     public void OpenOptionsInterface()
     {
         BeginInterface(optionsInterface);
         optionSelectionInterface.SetActive(true); customCircuitsInterface.SetActive(false); guideInterface.SetActive(false);
     }
 
+    /// <summary>
+    /// Opens the guide interface; called by pressing an in-scene button.
+    /// </summary>
     public void OpenGuide()
     {
         optionSelectionInterface.SetActive(false);
         guideInterface.SetActive(true);
     }
 
+    /// <summary>
+    /// Opens the custom circuit interface; called by pressing an in-scene button.
+    /// </summary>
     public void OpenCustomCircuits()
     {
         optionSelectionInterface.SetActive(false);
         customCircuitsInterface.SetActive(true);
     }
 
+    /// <summary>
+    /// Restores the text of a save slot to its default values after a success editor scene deletion.
+    /// </summary>
     public void SceneDeleteSubmission()
     {
         MenuSetupManager.Instance.DeleteEditorStructure(currentSceneIndex);
@@ -211,20 +325,26 @@ public class MenuInterfaceManager : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Checks the name in <seealso cref="sceneNameInputField"/> and creates an editor scene if it is valid.
+    /// </summary>
     public void SceneNameSubmission()
     {
         string submission = sceneNameInputField.text.ToLower().Trim();
 
+        // Cannot be empty
         if (submission == string.Empty) sceneNameError.text = "scene name must be non-empty";
 
+        // Must be unique
         else if (CurrentSceneNames.Contains(submission)) sceneNameError.text = "scene name must be unique";
 
-        else
-        {
-            MenuLogicManager.Instance.CreateScene(currentSceneIndex, submission);
-        }
+        // Creates and opens the editor structure.
+        else MenuLogicManager.Instance.CreateScene(currentSceneIndex, submission);
     }
 
+    /// <summary>
+    /// Closes the current interface in use.
+    /// </summary>
     public void CancelCurrentSubmission()
     {
         if (currentInterface == sceneNameInterface) sceneNameInputField.text = sceneNameError.text = "";
@@ -236,11 +356,13 @@ public class MenuInterfaceManager : MonoBehaviour
         enabled = false;
     }
 
-    public void Quit()
-    {
-        Application.Quit();
-    }
+    // Exits the game; called by pressing an in-scene button.
+    public void Quit() { Application.Quit(); }
 
+    /// <summary>
+    /// Opens an interface.
+    /// </summary>
+    /// <param name="newInterface">The interface to open.</param>
     private void BeginInterface(GameObject newInterface)
     {
         if (currentInterface != null) return;
@@ -251,7 +373,10 @@ public class MenuInterfaceManager : MonoBehaviour
         enabled = true;
     }
 
-    // Helper method
+    /// <summary>
+    /// Sets the visibility of the current interface.
+    /// </summary>
+    /// <param name="visible">Whether the current interface should be visible.</param>
     private void ToggleCurrentInterface(bool visible)
     {
         currentInterface.SetActive(visible); transparentBackground.SetActive(visible);
@@ -260,6 +385,9 @@ public class MenuInterfaceManager : MonoBehaviour
     // Getter methods
     public static MenuInterfaceManager Instance { get { return instance; } }
 
+    /// <summary>
+    /// Returns the named list of all editor structures.
+    /// </summary>
     private List<string> CurrentSceneNames
     {
         get
--- Unity Project/Assets/Scripts/MenuLogicManager.cs
+++ Unity Project/Assets/Scripts/MenuLogicManager.cs
@@ -3,68 +3,113 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// MenuLogicManager handles all scene transitions as well as validation tests for deleting custom circuits.
+/// </summary>
 public class MenuLogicManager : MonoBehaviour
 {
+    // Singleton state reference
     private static MenuLogicManager instance;
 
+    /// <summary>
+    /// Whether the current editor scene has just been created or not.<br/><br/>
+    /// If it is the first time, this value indicates that default values must be initialized in the editor scene.
+    /// </summary>
     private bool firstOpen;
 
+    /// <summary>
+    /// The index of the current editor scene to open.
+    /// </summary>
     private int currentSceneIndex;
 
+    /// <summary>
+    /// The current preview structure in the preview scene.
+    /// </summary>
     private PreviewStructure currentPreviewStructure;
 
+    /// <summary>
+    /// Utilized within <seealso cref="TraversalTest(PreviewStructure, PreviewStructure)"/> to sort all preview structures into these respective lists.<br/>
+    /// If a circuit is invalid, it means that it is being utilized somewhere and cannot be deleted. A valid circuit can be deleted.
+    /// </summary>
     private List<PreviewStructure> invalidCircuits, validCircuits;
 
+    // Enforces a singleton state pattern
     private void Awake()
     {
         if (instance != null)
         {
             Destroy(this);
-            throw new Exception("MenuManager instance already established; terminating.");
+            throw new Exception("MenuLogicManager instance already established; terminating.");
         }
 
         instance = this;
     }
 
+    /// <summary>
+    /// Attempts to open an editor scene by index.
+    /// </summary>
+    /// <param name="sceneIndex">Index of the editor structure to open.</param>
     public void OpenScene(int sceneIndex)
     {
         bool isCreated = MenuSetupManager.Instance.EditorStructures[sceneIndex] != null;
 
         firstOpen = !isCreated;
 
+        // Opens the scene only if it has not been just created; otherwise completes the setup process.
         if (isCreated) ImportScene(sceneIndex); else MenuInterfaceManager.Instance.BeginSceneNameSubmission(sceneIndex);
     }
 
+    /// <summary>
+    /// Creates an editor scene.
+    /// </summary>
+    /// <param name="sceneIndex">Index of the editor structure to create.</param>
+    /// <param name="name">Name of the editor structure to create.</param>
     public void CreateScene(int sceneIndex, string name)
     {
         MenuSetupManager.Instance.EditorStructures[sceneIndex] = new EditorStructure(name);
         ImportScene(sceneIndex);
     }
 
+    /// <summary>
+    /// Opens a preview structure.
+    /// </summary>
+    /// <param name="previewStructure">The preview structure to open.</param>
     public void OpenPreview(PreviewStructure previewStructure)
     {
         currentPreviewStructure = previewStructure;
         SceneManager.LoadScene(2);
     }
 
+    /// <summary>
+    /// Opens a scene that has already been created beforehand.
+    /// </summary>
+    /// <param name="sceneIndex"></param>
     private void ImportScene(int sceneIndex)
     {
         currentSceneIndex = sceneIndex;
         SceneManager.LoadScene(1);
     }
-
+    
+    /// <summary>
+    /// Runs several validation tests that determine whether a custom circuit can be deleted.<br/><br/>.
+    /// If there are no error messages returned, then the custom circuit can be deleted.
+    /// </summary>
+    /// <param name="_previewStructure">The prospect custom circuit/preview structure to delete.</param>
+    /// <returns></returns>
     public static List<string> CanDeleteCustomCircuit(PreviewStructure _previewStructure)
     {
-        int id = _previewStructure.ID;
         List<string> errorMessages = new List<string>();
         instance.invalidCircuits = new List<PreviewStructure>(); instance.validCircuits = new List<PreviewStructure>();
 
+        // Marks each preview structure as either valid or invalid
         foreach (PreviewStructure previewStructure in MenuSetupManager.Instance.PreviewStructures) instance.TraversalTest(previewStructure, _previewStructure);
 
+        // Tests #1/#2: not placed or part of a bookmark directly (top-most custom circuit) or indirectly (inside of another custom circuit) within an editor scene.
         foreach (EditorStructure editorStructure in MenuSetupManager.Instance.EditorStructures)
         {
             if (editorStructure == null) continue;
 
+            // Placed circuit test
             foreach (CircuitIdentifier circuitIdentifier in editorStructure.Circuits)
             {
                 if (circuitIdentifier.previewStructureID == -1) continue;
@@ -74,6 +119,7 @@ public class MenuLogicManager : MonoBehaviour
                 if (instance.invalidCircuits.Contains(previewStructure)) { errorMessages.Add("Circuit is directly and/or indirectly referenced by a placed circuit in an editor scene"); break; }
             }
 
+            // Bookmark test
             foreach (int customCircuitID in editorStructure.BookmarkIDs)
             {
                 if (customCircuitID == -1) continue;
@@ -84,6 +130,7 @@ public class MenuLogicManager : MonoBehaviour
             }
         }
 
+        // Test #3: not directly (top-most custom circuit) or indirectly (inside of another custom circuit) within a custom circuit.
         foreach (PreviewStructure previewStructure in MenuSetupManager.Instance.PreviewStructures)
         {
             if (previewStructure == _previewStructure) continue;
@@ -91,15 +138,29 @@ public class MenuLogicManager : MonoBehaviour
             if (instance.invalidCircuits.Contains(previewStructure)) { errorMessages.Add("Circuit is directly and/or indirectly referenced by another custom circuit"); break; }
         }
 
+        // Returns the list of all obtained error messages, if any.
         return errorMessages;
     }
 
+    /// <summary>
+    /// Sorts the current preview structure as invalid (contains the target circuit) or valid by running a modified depth-first search.<br/><br/>
+    /// Essentially, the user inputs a starting preview structure. Until a starting circuit (e.g. AND) is reached or there is nothing else to explore, this method:<br/>
+    /// - Recursively calls this method on all of its circuit components that are custom circuits granted they have not already been sorted into valid or invalid circuits.
+    /// - Adds the current circuit as an invalid preview structure if any of its children contained the target custom circuit, and returns.<br/>
+    /// - Adds the current circuit as a valid preview structure if none of its child custom circuits contained the target custom circuit.
+    /// </summary>
+    /// <param name="current"></param>
+    /// <param name="target"></param>
     private void TraversalTest(PreviewStructure current, PreviewStructure target)
     {
+        // If the target preview structure was recursively called by TraversalTest, then add it.
         if (current == target) { if (!invalidCircuits.Contains(current)) { invalidCircuits.Add(current); } return; }
 
+        // If already designated as an invalid/valid circuit, return.
         else if (invalidCircuits.Contains(current) || validCircuits.Contains(current)) return;
 
+        // Traverse through each custom circuit and recursively call this method.
+        // Once done, check to see if the target preview structure was detected. If so, then also add this preview structure.
         foreach (CircuitIdentifier circuitIdentifier in current.Circuits)
         {
             if (circuitIdentifier.previewStructureID != -1)
@@ -112,13 +173,13 @@ public class MenuLogicManager : MonoBehaviour
             }
         }
 
+        // If not invalid, then must be valid.
         validCircuits.Add(current);
     }
 
-    // Singleton state reference
+    // Getter methods
     public static MenuLogicManager Instance { get { return instance; } }
 
-    // Getter methods
     public bool FirstOpen { get { return firstOpen; } }
 
     public int CurrentSceneIndex { get { return currentSceneIndex; } }
